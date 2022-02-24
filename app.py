1563b7eea140a6c48d8dd8e48d726a4a49aa0a61
T-Platz <48485363+T-Platz@users.noreply.github.com>
2022-02-24 02:58:57 +0100

    Removed unnecessary template parameters

--- app.py
+++ app.py
@@ -12,12 +12,12 @@ def inject_now():
 
 @app.route('/')
 def home():
-    return render_template('home.html', pageName='Home', active='home')
+    return render_template('home.html')
 
 
 @app.route('/cv/')
 def cv():
-    return render_template('cv.html', pageName='CV', active='cv')
+    return render_template('cv.html')
 
 
 @app.route('/projects/', defaults={'tag': None})
@@ -26,7 +26,7 @@ def projects(tag):
     file = app.open_resource('static/json/projects.json')
     projectsList = [p for p in json.loads(file.read()) if tag is None or ('tags' in p and tag in p['tags'])]
     file.close()
-    return render_template('projects.html', pageName='Projects', active='projects', projectsList=projectsList, tag=tag)
+    return render_template('projects.html', projectsList=projectsList, tag=tag)
 
 
 if __name__ == '__main__':
